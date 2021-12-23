c727d5859cb3e99a80cd2535bdcc1d7a9e10e237
yseto-icd <62240287+yseto-icd@users.noreply.github.com>
2021-12-23 14:34:41 +0900

    Initial commit

---
+++ src/test/kotlin/org/example/detekt/MyRuleTest.kt
@@ -0,0 +1,55 @@
+package org.example.detekt
+
+import com.google.common.truth.Truth.assertThat
+import io.github.detekt.test.utils.KotlinCoreEnvironmentWrapper
+import io.github.detekt.test.utils.createEnvironment
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.test.compileAndLintWithContext
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.Test
+
+internal class CustomRuleSpec {
+
+    @Test
+    fun `reports inner classes`() {
+        val code = """
+        class A {
+          inner class B
+        }
+        """
+        val findings = MyRule(Config.empty).compileAndLintWithContext(env, code)
+        assertThat(findings).hasSize(1)
+    }
+
+    @Test
+    fun `doesn't report inner classes`() {
+        val code = """
+        class A {
+          class B
+        }
+        """
+        val findings = MyRule(Config.empty).compileAndLintWithContext(env, code)
+        assertThat(findings).isEmpty()
+    }
+
+    private val env: KotlinCoreEnvironment
+        get() = envWrapper.env
+
+    companion object {
+        private lateinit var envWrapper: KotlinCoreEnvironmentWrapper
+
+        @BeforeClass
+        @JvmStatic
+        fun setUp() {
+            envWrapper = createEnvironment()
+        }
+
+        @AfterClass
+        @JvmStatic
+        fun tearDown() {
+            envWrapper.dispose()
+        }
+    }
+}
